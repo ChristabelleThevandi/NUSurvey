064c057cf341a7fd604c653f5986d98902d20e6b
DaisyLiuSitong <e0376945@u.nus.edu>
2021-04-02 23:14:34 +0800

    create restful resources

---
+++ NusurveyRws/src/java/ws/rest/ApplicationConfig.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ws.rest;
+
+import java.util.Set;
+
+/**
+ *
+ * @author hp
+ */
+@javax.ws.rs.ApplicationPath("Resources")
+public class ApplicationConfig extends javax.ws.rs.core.Application {
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> resources = new java.util.HashSet<>();
+        addRestResourceClasses(resources);
+        return resources;
+    }
+
+    /**
+     * Do not modify addRestResourceClasses() method.
+     * It is automatically populated with
+     * all resources defined in the project.
+     * If required, comment out calling this method in getClasses().
+     */
+    private void addRestResourceClasses(Set<Class<?>> resources) {
+        resources.add(ws.rest.AnswerResource.class);
+        resources.add(ws.rest.CorsFilter.class);
+        resources.add(ws.rest.CreditCardResource.class);
+        resources.add(ws.rest.OptionResource.class);
+        resources.add(ws.rest.QuestionResource.class);
+        resources.add(ws.rest.ResponseResource.class);
+        resources.add(ws.rest.SurveyResource.class);
+        resources.add(ws.rest.TagResource.class);
+        resources.add(ws.rest.TransactionResource.class);
+        resources.add(ws.rest.UserResource.class);
+    }
+    
+}
---
+++ NusurveyRws/src/java/ws/rest/UserResource.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ws.rest;
+
+import ejb.session.stateless.UserSessionBeanLocal;
+import entity.User;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Produces;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PUT;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * REST Web Service
+ *
+ * @author hp
+ */
+@Path("User")
+public class UserResource {
+
+    UserSessionBeanLocal userSessionBean = lookupUserSessionBeanLocal();
+
+    @Context
+    private UriInfo context;
+
+    /**
+     * Creates a new instance of UserResource
+     */
+    public UserResource() {
+    }
+
+   
+    private UserSessionBeanLocal lookupUserSessionBeanLocal() {
+        try {
+            javax.naming.Context c = new InitialContext();
+            return (UserSessionBeanLocal) c.lookup("java:global/Nusurvey/Nusurvey-ejb/UserSessionBean!ejb.session.stateless.UserSessionBeanLocal");
+        } catch (NamingException ne) {
+            Logger.getLogger(getClass().getName()).log(Level.SEVERE, "exception caught", ne);
+            throw new RuntimeException(ne);
+        }
+    }
+}
